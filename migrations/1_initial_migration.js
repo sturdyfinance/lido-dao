95c52e8e860c9eef157c50ca4ddcb517332982d1
vipinrat92 <vipinrat92@gmail.com>
2021-10-22 04:16:24 +0530

    Migrations

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
-const Migrations = artifacts.require("Migrations");
+const Assignment = artifacts.require('Assignment');
+const Lido = '0xA5d26F68130c989ef3e063c9bdE33BC50a86629D';
+const stETH = '0xA0cA1c13721BAB3371E0609FFBdB6A6B8e155CC0';
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function (deployer, network, accounts) {
+  await deployer.deploy(Assignment,Lido,stETH);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -6,7 +6,6 @@ module.exports = {
    host: '127.0.0.1', // Localhost (default: none)
    port: 8545, // Standard Ethereum port (default: none)
    network_id: '*', // Any network (default: none)
-   gas: 12000000,
    from: process.env.OWNER,
   },
   ropsten: {
@@ -33,8 +32,9 @@ module.exports = {
     provider: () => 
        new HDWalletProvider(process.env.MNEMONIC, process.env.GOERLI),
     network_id: 5, // eslint-disable-line camelcase
-    gas: 12000000,
+    gas: 6721975,
     gasPrice: 10000000000,
+    skipDryRun: true,
   },
   bsctestnet: {
    provider: () =>
